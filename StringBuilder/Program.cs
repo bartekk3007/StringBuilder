c3f760e17d38ad83349bfa92a0d7d77350c6315b
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-17 23:30:49 +0200

    Add files via upload

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace StringBuilding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < 100; i++)
+            {
+                stringBuilder.Append($"{i.ToString()}\t");
+            }
+            string result = stringBuilder.ToString();
+            Console.WriteLine(result);
+        }
+    }
+}
